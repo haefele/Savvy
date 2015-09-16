94a5b261e1bf086ccbd0bd63071b6bf26d9d11d3
Daniel Häfele <haefele@xemio.net>
2015-09-16 19:09:52 +0200

    Add nice header formatting to the transactions view and improve the display format of the transaction amount

---
+++ src/Savvy/Converter/DecimalToStringWithTwoDecimalsConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Savvy.Converter
+{
+    public class DecimalToStringWithTwoDecimalsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var dec = (decimal)value;
+            return dec.ToString("N");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
